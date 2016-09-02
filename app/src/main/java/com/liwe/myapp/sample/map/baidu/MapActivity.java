07924d526e2f00f96e81439b69cdb285880285ee
v7428477 <2509398153@qq.com>
2016-09-02 14:33:47 +0800

    准备工作，各厂家 地图api 接入

---
+++ app/src/main/java/com/liwe/myapp/sample/map/baidu/MapActivity.java
@@ -0,0 +1,32 @@
+package com.liwe.myapp.sample.map.baidu;
+
+import android.content.Context;
+
+import com.liwe.myapp.base.BaseActivity;
+
+/**
+ * Date： 2016/9/2.
+ * Created by： v7428477
+ * email：764213580@qq.com
+ */
+public class MapActivity extends BaseActivity {
+    @Override
+    public int bindLayout() {
+        return 0;
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
