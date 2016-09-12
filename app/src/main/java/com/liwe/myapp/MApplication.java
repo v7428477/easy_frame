8d75dc7619bf1ca873bb72df3d66edabd1ebb647
v7428477 <2509398153@qq.com>
2016-09-12 14:34:58 +0800

    test

--- app/src/main/java/com/liwe/myapp/MApplication.java
+++ app/src/main/java/com/liwe/myapp/MApplication.java
@@ -10,6 +10,7 @@ import com.baidu.mapapi.SDKInitializer;
  * Date： 2016/8/30.
  * Created by： v7428477
  * email：764213580@qq.com
+ * 测试 新电脑github上传
  */
 public class MApplication extends Application {//Application：用来管理应用程序的全局状态。在应用程序启动时Application会首先创建，然后才会根据情况(Intent)来启动相应的Activity和Service。本示例中将在自定义加强版的Application中注册未捕获异常处理器。
     /** 对外提供整个应用生命周期的Context **/
